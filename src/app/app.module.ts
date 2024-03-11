d58a6c9c8ddfe2df92ff5ad7a4306163270e006c
vuslattt <vuslatcoskun2001@gmail.com>
2024-03-11 23:59:58 +0300

    mj

---
+++ src/app/app.module.ts
@@ -0,0 +1,68 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Route } from '@angular/router';
+import { AppComponent } from './app.component';
+
+import { ViewsModule } from './views/views.module';
+import { SharedModule } from './shared/shared.module';
+import { ErrorModule } from './views/errors/error.module';
+
+import { ModalsComponent } from './views/modals/modals.component';
+import { BasicTableComponent } from './views/tables/basic-table/basic-table.component';
+import { Profile1Component } from './views/profile/profile1/profile1.component';
+import { NotFoundComponent } from './views/errors/not-found/not-found.component';
+import { Dashboard1Component } from './views/dashboards/dashboard1/dashboard1.component';
+
+// main layout
+import { NavigationModule } from './main-layout/navigation/navigation.module';
+
+const routes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboards/v1' },
+  { path: 'dashboards', children:
+    [
+      { path: 'v1', component: Dashboard1Component },
+    ]
+  },
+  { path: 'profiles', children:
+    [
+      { path: 'profile1', component: Profile1Component },
+    ]
+  },
+  { path: 'tables', children:
+    [
+      { path: 'table1', component: BasicTableComponent },
+    ]
+  },
+  { path: 'drpanel', children:
+    [
+      { path: 'table1', component: BasicTableComponent },
+    ]
+  },
+  { path: 'modals', component: ModalsComponent},
+  { path: '**', component: NotFoundComponent },
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NavigationModule,
+    RouterModule.forRoot(routes, {}),
+    FormsModule,
+    SharedModule,
+    ViewsModule,
+    ErrorModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA ]
+})
+export class AppModule { }
